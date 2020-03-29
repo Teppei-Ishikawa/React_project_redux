7ea0f6efd584bf576d349d02301cb0501c792104
tepepe61 <ishikawa@temona.co.jp>
2020-03-29 15:42:24 +0900

    component作成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          へろーわーるど
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </div>
-  );
+  )
+}
+
+const Cat = () => {
+  return <div>ねこだよ-</div>
+
 }
 
 export default App;
